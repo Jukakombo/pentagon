128dce967d798c88bdff9185e5257e9c095c2279
jukakombo <alisonmagid2016@gmail.com>
2022-09-21 23:53:33 +0100

    adding

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://jubilee-academy.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://pentagon-school-juba.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
