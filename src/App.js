c96bd3224eb84ad2b23f8056ce84f6cd20340916
jukakombo <alisonmagid2016@gmail.com>
2022-09-24 21:17:54 +0100

    add all

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import React,{ useEffect, useState } from 'react';
-import Markup from './markup/Markup';
+import React, { useEffect, useState } from "react";
+import Markup from "./markup/Markup";
 import useScrollPosition from "use-scroll-position";
-import './css/plugins.css';
-import './css/style.css';
-import './css/templete.css';
-import './css/skin/skin-1.css';
-import './plugins/slick/slick.min.css';
-import './plugins/slick/slick-theme.min.css';
-import 'react-modal-video/css/modal-video.min.css';
-
+import "./css/plugins.css";
+import "./css/style.css";
+import "./css/templete.css";
+import "./css/skin/skin-1.css";
+import "./plugins/slick/slick.min.css";
+import "./plugins/slick/slick-theme.min.css";
+import "react-modal-video/css/modal-video.min.css";
+import ClockLoader from "react-spinners/ClockLoader";
 function App() {
-	const [body_, setbody_] = useState();
-	const [header, setHeader] = useState("fixed");
-	const [header_, setHeader_] = useState();
-	let scrollPosition = useScrollPosition();
+  const [body_, setbody_] = useState();
+  const [header, setHeader] = useState("fixed");
+  const [header_, setHeader_] = useState();
+  const [loading, setLoading] = useState(false);
+  let scrollPosition = useScrollPosition();
 
-	useEffect(() => {
-		setbody_(document.querySelector("body"));
-		setHeader_(document.getElementsByClassName("main-bar-wraper"));
-	  }, []);
+  useEffect(() => {
+    setbody_(document.querySelector("body"));
+    setHeader_(document.getElementsByClassName("main-bar-wraper"));
+  }, []);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
 
+  var element = document.getElementById("fix-header");
 
-	var element = document.getElementById("fix-header"); 	
-		
-	if(typeof(element) != 'undefined' && element != null){
-		header === "fixed" && scrollPosition > 10
-			? header_ && header_[0].classList.add("is-fixed")
-			: header_ && header_[0].classList.remove("is-fixed"); 	
-	}
+  if (typeof element != "undefined" && element != null) {
+    header === "fixed" && scrollPosition > 10
+      ? header_ && header_[0].classList.add("is-fixed")
+      : header_ && header_[0].classList.remove("is-fixed");
+  }
   return (
     <div className="App">
+       
       <Markup />
     </div>
   );
