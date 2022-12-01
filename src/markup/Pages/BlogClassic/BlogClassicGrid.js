fdb34ea81a5290695e544c4cd7e1dfc3d5f9e53e
Jukakombo <alisonmagid2016@gmail.com>
2022-12-01 22:26:28 +0100

    aad

--- src/markup/Pages/BlogClassic/BlogClassicGrid.js
+++ src/markup/Pages/BlogClassic/BlogClassicGrid.js
@@ -31,15 +31,15 @@ const masonryOptions = {
 const imagesLoadedOptions = { background: ".my-bg-image-el" };
 // Masonry section end
 
-function BlogClassicGrid( ) {
-	const { id } = useParams();
+function BlogClassicGrid() {
+  const { id } = useParams();
 
-	const [articles, setarticles] = useState([]);
-  
-	useEffect(() => {
-	  client
-		.fetch(
-		  `*[_type=="studentsArticles"]{
+  const [articles, setarticles] = useState([]);
+
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type=="studentsArticles"]{
 		title, body, imageUrl{
 		  asset -> {
 			_id,
@@ -48,11 +48,11 @@ function BlogClassicGrid( ) {
 		},
 		author,_id
 	  }`
-		)
-		.then((data) => {
-		  setarticles(data);
-		});
-	}, [id]);
+      )
+      .then((data) => {
+        setarticles(data);
+      });
+  }, [id]);
   return (
     <>
       <Header />
@@ -69,63 +69,67 @@ function BlogClassicGrid( ) {
         <div className="content-area">
           <div className="container">
             <div className="dlab-blog-grid-3 " id="masonry">
-            {articles.length>0 ? (<Masonry
-                className={"my-gallery-class"} // default ''
-                options={masonryOptions} // default {}
-                disableImagesLoaded={false} // default false
-                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-                imagesLoadedOptions={imagesLoadedOptions} // default {}
-              >
-                {articles.map((data, index) => (
-                  <div
-                    className="post card-container col-lg-4 col-md-6 col-sm-12 col-xs-12"
-                    key={index}
-                  >
-                    <div className="blog-post blog-grid blog-rounded blog-effect1">
-                      <div className="dlab-post-media dlab-img-effect ">
-                        <Link to={"#"}>
-                          <img src={data?.imageUrl?.asset?.url} alt="" />
-                        </Link>{" "}
-                      </div>
-                      <div className="dlab-info p-a20 border-1">
-                        <div className="dlab-post-title ">
-                          <h4 className="post-title">
-                            <Link to={"#"}>{data?.title}</Link>
-                          </h4>
+              {articles.length > 0 ? (
+                <Masonry
+                  className={"my-gallery-class"} // default ''
+                  options={masonryOptions} // default {}
+                  disableImagesLoaded={false} // default false
+                  updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                  imagesLoadedOptions={imagesLoadedOptions} // default {}
+                >
+                  {articles.map((data, index) => (
+                    <div
+                      className="post card-container col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                      key={index}
+                    >
+                      <div className="blog-post blog-grid blog-rounded blog-effect1">
+                        <div className="dlab-post-media dlab-img-effect ">
+                          <Link to={"#"}>
+                            <img src={data?.imageUrl?.asset?.url} alt="" />
+                          </Link>{" "}
                         </div>
-                        <div className="dlab-post-meta">
-                          <ul className="d-flex align-items-center">
-                            <li className="post-date">
-                              {" "}
-                              <i className="fa fa-calendar"></i>
-                              <strong>{moment(data?._createdAt).format('ll')}</strong>  {" "}
-                            </li>
-                            <li className="post-author">
-                              <i className="fa fa-user"></i>By{" "}
-                              <Link to={"#"}>{data?.author}</Link>{" "}
-                            </li>
-                            <li className="post-comment">
-                              <i className="fa fa-comments"></i>{" "}
-                              <Link to={"#"}>0</Link>{" "}
-                            </li>
-                          </ul>
-                        </div>
-                        <div className="dlab-post-text">
-                          <p>
-                            {data?.title} was written by our student {data?.author}
-                          </p>
-                        </div>
-                        <div className="dlab-post-readmore blog-share">
-                          <Link
-                            to={`/${data?._id}`}
-                            title="READ MORE"
-                            rel="bookmark"
-                            className="site-button outline outline-1"
-                          >
-                            READ MORE
-                            <i className="fa fa-long-arrow-right ml-1"></i>
-                          </Link>
-                          {/* <div className="share-btn">
+                        <div className="dlab-info p-a20 border-1">
+                          <div className="dlab-post-title ">
+                            <h4 className="post-title">
+                              <Link to={"#"}>{data?.title}</Link>
+                            </h4>
+                          </div>
+                          <div className="dlab-post-meta">
+                            <ul className="d-flex align-items-center">
+                              <li className="post-date">
+                                {" "}
+                                <i className="fa fa-calendar"></i>
+                                <strong>
+                                  {moment(data?._createdAt).format("ll")}
+                                </strong>{" "}
+                              </li>
+                              <li className="post-author">
+                                <i className="fa fa-user"></i>By{" "}
+                                <Link to={"#"}>{data?.author}</Link>{" "}
+                              </li>
+                              <li className="post-comment">
+                                <i className="fa fa-comments"></i>{" "}
+                                <Link to={"#"}>0</Link>{" "}
+                              </li>
+                            </ul>
+                          </div>
+                          <div className="dlab-post-text">
+                            <p>
+                              {data?.title} was written by our student{" "}
+                              {data?.author}
+                            </p>
+                          </div>
+                          <div className="dlab-post-readmore blog-share">
+                            <Link
+                              to={`/${data?._id}`}
+                              title="READ MORE"
+                              rel="bookmark"
+                              className="site-button outline outline-1"
+                            >
+                              READ MORE
+                              <i className="fa fa-long-arrow-right ml-1"></i>
+                            </Link>
+                            {/* <div className="share-btn">
                             <ul className="clearfix">
                               <li>
                                 <Link to={`/${}`} className="site-button sharp">
@@ -154,13 +158,15 @@ function BlogClassicGrid( ) {
                               </li>
                             </ul>
                           </div> */}
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
-              </Masonry>) :"Loading..."}
-              
+                  ))}
+                </Masonry>
+              ) : (
+                "Loading..."
+              )}
             </div>
             <div className="pagination-bx clearfix text-center">
               <ul className="pagination">
