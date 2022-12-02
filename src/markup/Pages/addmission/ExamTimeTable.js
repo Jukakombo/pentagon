4b91fb4ec96ceead389fc09638b99df7cc2be5b3
Jukakombo <alisonmagid2016@gmail.com>
2022-12-02 11:20:40 +0100

    add s

--- src/markup/Pages/addmission/ExamTimeTable.js
+++ src/markup/Pages/addmission/ExamTimeTable.js
@@ -10,11 +10,27 @@ function ExamTimeTable() {
     <>
       <Header />
       <div
-            className="dlab-bnr-inr overlay-primary"
-            style={{ backgroundImage: "url(" + bnr + ")" }}
-          >
-            <PageTitle motherMenu="Exam Timetable " activeMenu="Exam Timetable " />
-          </div>
+        className="dlab-bnr-inr overlay-primary"
+        style={{ backgroundImage: "url(" + bnr + ")" }}
+      >
+        <PageTitle motherMenu="Exam Timetable " activeMenu="Exam Timetable " />
+      </div>
+
+      <div className="container">
+        <div className="section-head text-black text-center">
+          <h4 className="text-gray-dark m-b10">About </h4>
+          <h2 className="box-title m-tb0">
+            Examination time table
+            <span className="bg-primary"></span>
+          </h2>
+          <p>
+            This exam time is set by the school administration, and all students
+            are expected to respect and prepare for their exams as the exam date
+            approaches.
+          </p>
+        </div>
+      </div>
+
       <div className="container">
         <div className="section-head text-center">
           {/* <div className=" row dzseth  m-b30"> */}
